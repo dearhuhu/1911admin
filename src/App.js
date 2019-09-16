e9aa2436b2d9864aa2a8ca09f81c6752efa5f137
gaoss-huhu <1348206093@qq.com>
2019-09-16 10:25:14 +0800

    projectinit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import'../src/style/test.less'
+import { Button } from 'antd';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button type="danger">hehe</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'antd/dist/antd.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/antd.css';
+import axios from '../src/utils/axios.js'
+React.Component.prototype.$axios=axios
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
